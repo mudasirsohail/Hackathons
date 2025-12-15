58878bc05897aa8386bf887a7ccc1364e051f35f
unknown <mudasirsohailsoomro@gmail.com>
2025-12-15 13:04:54 +0500

    Fix async SQLAlchemy engine

--- backend/app/database/db.py
+++ backend/app/database/db.py
@@ -1,23 +1,38 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
 from ..config.settings import settings
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
 
 
 # Create the database engine
-engine = create_engine(settings.NEON_DB_URL)
+# engine = create_engine(settings.NEON_DB_URL)
+engine = create_async_engine(
+    settings.NEON_DB_URL,
+    echo=False,
+)
+AsyncSessionLocal = sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
 
 # Create a SessionLocal class
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Base class for models
-Base = declarative_base()
+# Base = declarative_base()
 
 
-def get_db():
-    """Dependency to get database session"""
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+# def get_db():
+#     """Dependency to get database session"""
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
